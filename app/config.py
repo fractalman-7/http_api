bad412f0479e95be411c892be4b7fb29c98971a9
fractalman-7 <fractalman.id7@gmail.com>
2021-08-19 15:57:16 +0300

    Since there is a chance not to connect to the site, a reconnection is added

--- app/config.py
+++ app/config.py
@@ -1,2 +1,4 @@
 CURRENCY_CODES_URL = "https://www.cbr.ru/scripts/XML_valFull.asp"
 CURRENCY_RATES_URL = "https://www.cbr.ru/scripts/XML_daily.asp"
+
+NUMBER_OF_CONNECTION_ATTEMPTS = 3
--- app/utils.py
+++ app/utils.py
@@ -1,19 +1,30 @@
 from typing import Optional
 from urllib.parse import urlparse
 from xml.etree import ElementTree
+from asyncio import sleep
 
 from aiohttp import ClientSession, ClientConnectionError
 from fastapi import HTTPException
 
+from app.config import NUMBER_OF_CONNECTION_ATTEMPTS
+
 
 async def fetch_raw_data(url: str, params: dict[str, str] = None) -> str:
     async with ClientSession() as client:
-        try:
-            async with client.get(url, params=params) as response:
-                return await response.text()
-        except ClientConnectionError:
-            hostname = urlparse(url).hostname
-            raise HTTPException(400, f"Failed to connection with {hostname}")
+        connected = False
+        attempt = 0
+        while not connected:
+            try:
+                async with client.get(url, params=params) as response:
+                    connected = True
+                    return await response.text()
+            except ClientConnectionError:
+                attempt += 1
+            if attempt > NUMBER_OF_CONNECTION_ATTEMPTS:
+                break
+            await sleep(0.3)
+        hostname = urlparse(url).hostname
+        raise HTTPException(400, f"Failed to connection with {hostname}")
 
 
 def parse_currency_codes(raw_data: str) -> set[str]:
