27a297f596a29c9105e1028bda55e091bb7be1ae
fractalman-7 <fractalman.id7@gmail.com>
2021-08-19 13:10:53 +0300

    Updated request handlers

--- app.py
+++ app.py
@@ -1,21 +1,28 @@
 from datetime import date
 
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Query
 
 from services import get_currency_codes, get_currency_rate
 
 app = FastAPI()
 
 
-@app.get("/currency_codes")
+@app.get("/currency_codes", response_model=list[str])
 async def currency_codes():
+    """Getting a list of all available currencies"""
+
     return await get_currency_codes()
 
 
-@app.get("/currency_rates_diff")
-async def currency_rates_diff(code: str, date1: date, date2: date):
+@app.get("/currency_rates_diff", response_model=int)
+async def currency_rates_diff(
+        code: str = Query(..., max_length=3, min_length=3, description="ISO currency code"),
+        date1: date = Query(..., description="First date"),
+        date2: date = Query(..., description="Second date")
+):
+    """Getting the difference in the currency rate between two dates"""
+
     if date1 <= date2 and code in await get_currency_codes():
         diff = abs(await get_currency_rate(code, date1) - await get_currency_rate(code, date2))
         return diff
-    else:
-        raise HTTPException(status_code=400, detail="Invalid parameters")
+    raise HTTPException(status_code=400, detail="Invalid parameters")
